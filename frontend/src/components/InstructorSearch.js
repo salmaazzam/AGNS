07e44bd6ab2e65f611df9d0c05a32a6a13450ef2
Gannah Sherif <gannahsherifabdelsattar@gmail.com>
2022-11-06 14:00:26 -0800

    Frontend continuation
    more frontend

---
+++ frontend/src/components/InstructorSearch.js
@@ -0,0 +1,66 @@
+import { useEffect,useState } from 'react'
+import { useCoursesContext } from "../hooks/useCourseContext"
+import CourseDetails from '../components/CourseDetails'
+
+const InstructorSearch = () =>{
+const {dispatch} = useCoursesContext()
+const [info, setSearchKey]= useState('') 
+const [courses, setCourses]= useState('')
+const [emptyFields, setEmptyFields] = useState([])
+const [error, setError] = useState(null)
+    
+const handleSubmit = async (e) => {
+      e.preventDefault()
+    const info = {info}
+
+   const response = await fetch ('/course/63673640b44f1ebe24992530', {
+    method: 'POST',
+     body: JSON.stringify(info),
+      headers:{
+                'Content-Type': 'application/json'
+            }
+        })
+        const json = await response.json()
+        if(response.ok){
+            setCourses(json)
+            dispatch ({type: 'SET_COURSE', payload: json})
+        }
+
+        
+    }
+return(
+    <form className = "Search">
+        <div className = "search" onSubmit={handleSubmit}>
+        <label>Search My Courses</label>
+        <input type = "text"
+        onChange= {(e)=> setSearchKey(e.target.value)}
+        value = {info}
+        className={emptyFields.includes('key') ? 'error' : ''}
+        />
+        </div>
+        <div className = "filterPrice">
+        <p>filter Price</p> 
+        <input type= "number" placeholder='min'/>
+        <input type= "number" placeholder='max'/>
+        <button>Filter Price</button>
+        </div>
+
+        <div className = "filterSubject">
+        <p>filter Price</p> 
+        <input type= "textbox" placeholder='Subject'/>
+        <button>Filter Subject</button>
+        </div>
+        <div className='SearchResult'>
+            <h1>Search Results</h1>
+            {courses && courses.map(course => (
+          <CourseDetails course={course} key={course._id} />
+        ))}
+        </div>
+        <button>Search</button>
+        {error && <div className="error">{error}</div>}
+    </form>
+)
+
+}
+
+export default InstructorSearch
