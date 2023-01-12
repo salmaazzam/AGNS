ae99ece5ba966cc88906a318036dcbd13a5561e2
salmaazzam <salmaazzam119@gmail.com>
2023-01-12 23:08:16 +0100

    added setting status of report in backend

--- backend/Routes/Report.js
+++ backend/Routes/Report.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const Report = require("../Models/ReportSchema");
 const {addReport} =require('../Controllers/reportController');
 const { getReports } = require("../Controllers/reportController");
-
+const {setStatus} = require("../Controllers/reportController");
 
 
 
 router.post("/",addReport)
 router.get("/allreports",getReports)
+router.post("/updatereport",setStatus)
 
 
 
